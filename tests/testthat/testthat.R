28de5c54bebc856b8aa216737cdc3a86686ac37b
Clement Schaff <clement.schaff@gmail.com>
2017-07-18 11:30:57 +0200

    add tests for earthquakes plots

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -38,27 +38,31 @@ test_that("Noaa pkg tests", {
   # test for regular dates
   expect_that(eq_clean_test_table$DATE[2], is_identical_to(lubridate::ymd("2000-06-17")))
 
-#
-#   ggplot() +
-#     geom_timeline(data = eq_clean_test_table, aes(x = DATE, size = EQ_PRIMARY, y = COUNTRY, color = DEATHS)) +
-#     scale_size_continuous(name  ="Richter scale value") +
-#     scale_colour_continuous(name  ="# deaths") +
-#     theme_timeline
-#
-#   ggplot() +
-#     geom_timeline(data = eq_20e, aes(x = DATE, size = EQ_PRIMARY, color = DEATHS)) +
-#     geom_timeline_label(data = noteworthy_eq, aes(x = DATE, label = LOCATION_NAME)) +
-#     scale_size_continuous(name  ="Richter scale value") +
-#     scale_colour_continuous(name  ="# deaths") +
-#     theme_timeline
+  # test that geom_timeline generates a valid ggplot object
+  p1 <- ggplot() +
+    geom_timeline(data = eq_clean_test_table, aes(x = DATE, size = EQ_PRIMARY, y = COUNTRY, color = DEATHS)) +
+    scale_size_continuous(name  ="Richter scale value") +
+    scale_colour_continuous(name  ="# deaths") +
+    theme_timeline
 
-# ggplot() +
-#   geom_timeline(data = eq_20e, aes(x = DATE, y = COUNTRY, size = EQ_PRIMARY, color = DEATHS)) +
-#   geom_timeline_label(data = noteworthy_eq, aes(x = DATE, y = COUNTRY, label = LOCATION_NAME)) +
-#   scale_size_continuous(name  ="Richter scale value") +
-#   scale_colour_continuous(name  ="# deaths") +
-#   theme_timeline
-#
+  expect_is(p1, "ggplot")
+  expect_is(p1$layers[[1]]$geom, "GeomTimeline")
+
+  # test that geom_timeline_label generates a valid ggplot object
+  noteworthy_eq <- eq_clean %>%
+    filter(EQ_PRIMARY == max(EQ_PRIMARY, na.rm = T) | DEATHS == max(DEATHS, na.rm = T))
+
+  p2 <- ggplot() +
+    geom_timeline(data = eq_clean, aes(x = DATE, size = EQ_PRIMARY, color = DEATHS)) +
+    geom_timeline_label(data = noteworthy_eq, aes(x = DATE, label = LOCATION_NAME)) +
+    scale_size_continuous(name  ="Richter scale value") +
+    scale_colour_continuous(name  ="# deaths") +
+    theme_timeline
+
+  expect_is(p2, "ggplot")
+  expect_is(p2$layers[[2]]$geom, "GeomTimelineLabel")
+
+
 # readr::read_delim(eq_raw_file, delim = "\t") %>%
 #   eq_clean_data() %>%
 #   dplyr::filter(COUNTRY == "MEXICO" & lubridate::year(DATE) >= 2000) %>%
